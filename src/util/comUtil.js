7a3b672a5d24fe5f3de499b492d47d931caf82e2
changh <1154818557@qq.com>
2019-06-30 21:04:40 +0800

    搜索

--- src/util/comUtil.js
+++ src/util/comUtil.js
@@ -49,7 +49,7 @@ export default {
       'address': '成都市武侯区复城国际T4#1603',
       'fphone': '0825-12345678',
       'dzVipCardSupportFlag': 0,
-      'sessionId': '6d76e880f0e546baae8f50cfaa22f56b'
+      'sessionId': '01618bf32eb44e03aef27f090efe8d42'
     }
     let userInfo = {
       'nickName': '午後’品茶',
